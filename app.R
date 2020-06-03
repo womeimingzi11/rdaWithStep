f3d1b5bf33761ce1421131c883c5a7ee4454e368
Han Chen <chenhan28@gmail.com>
2020-06-03 18:25:01 +0800

    Add Overview page

--- app.R
+++ app.R
@@ -31,8 +31,7 @@ ui <- fluidPage(
     h5(
         a(href = "mailto://chenhan28@gmail.com", 'chenhan28@gmail.com')
     ),
-    h6('Update version: ',
-       Sys.Date()),
+    h6('Update version: 20600603'),
     sidebarLayout(
         sidebarPanel(
             p(
@@ -78,6 +77,35 @@ ui <- fluidPage(
             # )
         ),
         mainPanel(tabsetPanel(
+            tabPanel('Overview',
+                     div(
+                         h3('Wha is RDA with step selection?'),
+                     div(
+                         p(
+                             'Briefly, the Monte Carlo permutation tests followed by backward, forward or bothward selection were used to determine which variable was contained in each variable set.'
+                         ),
+                         p(
+                             'As',a(href = 'https://www.rdocumentation.org/packages/vegan/versions/2.4-2/topics/ordistep','vegan::ordistep'),
+                             'described：'
+                         )
+                     ),
+                     div(
+                         p('The basic functions for model choice in constrained ordination are add1.cca and drop1.cca. With these functions, ordination models can be chosen with standard R function step which bases the term choice on AIC. AIC-like statistics for ordination are provided by functions deviance.cca and extractAIC.cca (with similar functions for rda). Actually, constrained ordination methods do not have AIC, and therefore the step may not be trusted. This function provides an alternative using permutation P-values.'),
+                         p('Function ordistep defines the model, scope of models considered, and direction of the procedure similarly as step. The function alternates with drop and add steps and stops when the model was not changed during one step. The - and + signs in the summary table indicate which stage is performed. It is often sensible to have Pout > Pin in stepwise models to avoid cyclic adds and drops of single terms'),
+                         style = "color: blue")),
+                     div(
+                         h3('Focus on species or sample site?'),
+                         div(
+                             p('In gerneral, there is two main scopes of RDA: 1. determine the relationships of species and environment variables only; 2.except determine the relationships of species and environment variables, the simple sites were also considered.'),
+                             p('In this case, adding sample sites in the figure is not in my plan yet.'),
+                             p('However, you are welcomed to commit any feature about this and even any other features in my',
+                               a(href = "https://github.com/womeimingzi11/rdaWithStep",'repo on GitHub.')
+                             ),
+                             p('You are also welcomed to visited my',
+                               a(href = "https://womeimingzi11.github.io", 'Blog (in Chinese)'), ', or contact me by',
+                               a(href = "mailto://chenhan28@gmail.com", 'mail.'))
+                         )
+                     )),
             tabPanel(
                 'Species & Environment Matrix',
                 DTOutput('df_com'),
@@ -120,18 +148,18 @@ server <- function(input, output) {
     })
     output$df_com <- renderDataTable({
         if (df_com() == "") {
-            tribble( ~ spe_A, ~ spe_B,
-                     1, 2,
-                     3, 4)
+            tribble(~ spe_A, ~ spe_B,
+                    1, 2,
+                    3, 4)
         } else {
             df_com()
         }
     })
     output$df_env <- renderDataTable({
         if (df_env() == "") {
-            tribble( ~ env_A, ~ env_B,
-                     1, 2,
-                     3, 4)
+            tribble(~ env_A, ~ env_B,
+                    1, 2,
+                    3, 4)
         } else {
             df_env()
         }
